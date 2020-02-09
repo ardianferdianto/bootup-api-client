6a9c052b9c78e0f45b99c16de0d511eb9225de8a
kiddy <mhudyaramadhana@gmail.com>
2020-02-09 14:46:31 +0700

    Fixed discounts item.

--- src/Entity/DiscountsItem.php
+++ src/Entity/DiscountsItem.php
@@ -11,7 +11,7 @@ namespace BootUP\Client\Entity;
 
 class DiscountsItem extends BaseItem
 {
-    protected $type = 'discounts';
+    protected $type = 'discount';
     protected $availableRelations = [
 
     ];
