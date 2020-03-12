79a011d8a93ceafac46227cc4cc2a73a044a5872
ardianferdianto <ardianferdianto@gmail.com>
2020-03-12 15:36:37 +0700

    Add Section  Items

---
+++ src/Entity/CourseSectionItem.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ardianferdianto
+ * Date: 12/03/20
+ * Time: 15.35
+ */
+
+namespace BootUP\Client\Entity;
+
+
+class CourseSectionItem extends BaseItem
+{
+    protected $type = 'course_section';
+}
